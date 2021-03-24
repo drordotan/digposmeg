22e1679144bbbccd8d49650bb81a04941d3e01aa
Fosca <fosca.alroumi@gmail.com>
2021-03-24 20:56:40 +0100

    adjusted the paths for calmar and alambic

--- src/dpm/consts.py
+++ src/dpm/consts.py
@@ -1,15 +1,19 @@
 import socket
 import re
+import sys
 
 if re.match(".*dror.*", socket.gethostname(), re.IGNORECASE):
     base_path = '/Users/dror/meg/digit-position/exp/'
     results_dir = base_path + 'results/'
     raw_data_path = base_path + 'exp-data/'
     figures_path = '/Users/dror/data/acad-proj/2-InProgress/DigitPositionMEG/figures'
-elif re.match(".*fosca.*", socket.gethostname(), re.IGNORECASE):
-    base_path = '/Volumes/COUCOU_CFC/digitpos/exp/'
+elif re.match(".*calmar.*", socket.gethostname(), re.IGNORECASE) or re.match(".*alambic.*", socket.gethostname(), re.IGNORECASE):
+    base_path = '/neurospin/meg/meg_tmp/DPEM_Dror_Fosca_2017/'
     results_dir = base_path + 'results/'
     raw_data_path = base_path + 'exp-data/'
+    sys.path.append(base_path+'/scripts/')
+    sys.path.append(base_path+'/scripts/digposmeg/')
+    sys.path.append(base_path+'/scripts/umne/')
 else:
     raise Exception('Unrecognized computer')
 
