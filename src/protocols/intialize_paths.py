1c87161b062f6a24b86eba4803e1dc824a19a962
Fosca <fosca.alroumi@gmail.com>
2021-04-02 12:43:05 +0200

    adjusted the paths for working from NSpin network

--- src/protocols/intialize_paths.py
+++ src/protocols/intialize_paths.py
@@ -4,6 +4,10 @@ base_path = '/neurospin/meg/meg_tmp/DPEM_Dror_Fosca_2017/'
 sys.path.append(base_path + '/scripts/digposmeg/src/')
 
 import dpm
+import mne
+fif_path = 'neurospin/meg/meg_tmp/DPEM_Dror_Fosca_2017/exp-data/ag_170045/sss/comp1c_1_raw_sss.fif'
+mne.io.read_raw_fif(fif_path)
+
 
 results_dir = base_path + 'results/'
 raw_data_path = base_path + 'exp-data/'
