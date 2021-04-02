17996356f90633b9d42f99b25c7f7fee404d6566
Fosca <fosca.alroumi@gmail.com>
2021-04-02 12:02:31 +0200

    adjusted the paths for working from NSpin network

--- src/dpm/consts.py
+++ src/dpm/consts.py
@@ -9,13 +9,14 @@ if re.match(".*dror.*", socket.gethostname(), re.IGNORECASE):
     figures_path = '/Users/dror/data/acad-proj/2-InProgress/DigitPositionMEG/figures'
 elif re.match(".*calmar.*", socket.gethostname(), re.IGNORECASE) or re.match(".*alambic.*", socket.gethostname(), re.IGNORECASE):
     base_path = '/neurospin/meg/meg_tmp/DPEM_Dror_Fosca_2017/'
+    sys.path.append(base_path + '/scripts/digposmeg/src/')
     results_dir = base_path + 'results/'
     raw_data_path = base_path + 'exp-data/'
     sys.path.append(base_path + '/scripts/')
     sys.path.append(base_path + '/scripts/digposmeg/')
-    sys.path.append(base_path + '/scripts/digposmeg/src/')
     sys.path.append(base_path + '/scripts/umne/src/')
     sys.path.append('/neurospin/meg/meg_tmp/Geom_Seq_Fosca_2017/Geom_Seq_scripts/Analysis_scripts/packages/pyRiemann/')
+
 else:
     raise Exception('Unrecognized computer')
 
--- src/protocols/intialize_paths.py
+++ src/protocols/intialize_paths.py
@@ -2,7 +2,6 @@ import sys
 
 base_path = '/neurospin/meg/meg_tmp/DPEM_Dror_Fosca_2017/'
 sys.path.append(base_path + '/scripts/digposmeg/src/')
-
 results_dir = base_path + 'results/'
 raw_data_path = base_path + 'exp-data/'
 sys.path.append(base_path + '/scripts/')
